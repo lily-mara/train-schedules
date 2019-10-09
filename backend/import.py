e49432936c9e64c5379acded990612e32ff11211
Nate Mara <nate@onesignal.com>
2019-10-08 20:09:47 -0700

    Update data files

--- backend/import.py
+++ backend/import.py
@@ -7,6 +7,3 @@ print('.mode csv')
 
 for file in glob('data/*.txt'):
     print(f'.import {file} {os.path.splitext(os.path.basename(file))[0]}')
-
-print('alter table stop_times add column departure_minute integer;')
-print('alter table stop_times add column arrival_minute integer;')
