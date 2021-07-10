b7ff4803eef56eef938b87e64730014b98f09031
Lily Mara <lilymara@fastmail.com>
2021-07-10 13:55:52 -0700

    Delete ansible stuff

--- build-backend.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker-compose run --rm backend cargo build --release
-container=$(docker-compose run -d backend sleep 10000000)
-
-docker cp $container:/target/release/train-schedules .
-docker rm -f $container
