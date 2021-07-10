afc8163197558899d69ce4d8eb6a21a647cfebaa
Lily Mara <lilymara@fastmail.com>
2021-07-09 20:30:31 -0700

    Delete the data directory

--- backend/new-db.sh
+++ backend/new-db.sh
@@ -1,4 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
+
+mkdir -p data
+pushd data
+curl -Lo caltrain-ca-us.zip http://data.trilliumtransit.com/gtfs/caltrain-ca-us/caltrain-ca-us.zip
+unzip caltrain-ca-us.zip
+popd
 
 ./import.py | sqlite3 new-schedules.db
 PYTHONHASHSEED=0 ./migrate.py new-schedules.db
